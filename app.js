693ca45ae7cff21fffe6753e3021e6637fdfc718
fraserxu <xvfeng123@gmail.com>
2014-09-27 00:25:30 +0800

    save my progress

--- app.js
+++
@@ -1,9 +0,0 @@
-var static = require('node-static');
-
-var fileServer = new static.Server('./app');
-
-require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-        fileServer.serve(request, response);
-    });
-}).listen(process.env.VCAP_APP_PORT || 8080);
