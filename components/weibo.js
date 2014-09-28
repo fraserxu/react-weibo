d12ebf519e491f1b9b0b9bbeae3e14b876a1eee4
fraserxu <xvfeng123@gmail.com>
2014-09-28 09:32:31 +0800

    add weibo.js

---
+++ components/weibo.js
@@ -0,0 +1,19 @@
+var jsonp = require('jsonp');
+
+module.exports = {
+  getUid: function(cb) {
+    var uid_url = 'https://api.weibo.com/2/account/get_uid.json?access_token=2.00YwP8sBGoDixB537b1199b20i7RQn';
+    jsonp(uid_url, function(err, res) {
+      if(err) return cb(err)
+      cb(null, res.data.uid)
+    })
+  },
+
+  getUser: function(uid, cb) {
+    var user_url = 'https://api.weibo.com/2/users/show.json?access_token=2.00YwP8sBGoDixB537b1199b20i7RQn&uid=' + uid
+    jsonp(user_url, function(err, res) {
+      if(err) cb(err)
+      cb(null, res.data)
+    })
+  }
+}
