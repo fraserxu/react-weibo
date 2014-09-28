638f2b944c95bee54bcff2228836c09e523c0d5f
fraserxu <xvfeng123@gmail.com>
2014-09-28 09:32:42 +0800

    rewrite with browserify

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var React = require('react');
 // var axios = require('axios');
 // var Header = require('./components/header');
 // var Status = require('./components/status');
-var jsonp = require('jsonp');
+var weibo = require('./components/weibo.js');
 
 var APP = React.createClass({
   getInitialState: function() {
@@ -16,30 +16,31 @@ var APP = React.createClass({
   },
 
   componentWillMount: function() {
-    this.getUser()
+    this.getUser(function(err, user) {
+      if(err) alert('ger user error')
+      this.setState({
+        user: user
+      })
+    }.bind(this))
   },
 
-  getUser: function() {
-    var uid_url = 'https://api.weibo.com/2/account/get_uid.json?access_token=2.00YwP8sBGoDixB537b1199b20i7RQn';
-    jsonp(uid_url, function(err, res) {
-      var uid = res.data.uid
-      var user_url = 'https://api.weibo.com/2/users/show.json?access_token=2.00YwP8sBGoDixB537b1199b20i7RQn&uid=' + uid
-      jsonp(user_url, function(err, res) {
-        console.log('res', res)
-        this.setState({user: res.data})
-      }.bind(this))
-    }.bind(this))
+  getUser: function(cb) {
+    weibo.getUid(function(err, uid) {
+      weibo.getUser(uid, function(err, user) {
+        if(err) return cb(err)
+        cb(err, user)
+      })
+    })
   },
 
   render: function() {
     var data = JSON.stringify(this.state.user, null, 4)
     return (
       <div>
-        <p>This sucks!!!</p>
         <pre>{data}</pre>
       </div>
     );
   }
 });
 
-React.renderComponent(<APP />, document.body);
+React.renderComponent(<APP />, document.getElementById('main'));
